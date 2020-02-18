4a55efb86570c7c1f698c8a393fa33e6367a7715
Jason Linden <jason.linden@lexisnexis.co.za>
2020-02-18 16:19:32 +0200

    simple sort init and bubble sort completed algo.

---
+++ CSharp/BubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleSort
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var a = new SortBub(10);
+            a.Insert(8812);
+            a.Insert(45);
+            a.Insert(33);
+            a.Insert(88);
+            a.Insert(567);
+            a.Insert(142);
+            a.Insert(6764);
+            a.Insert(34);
+            a.Insert(123);
+            a.Insert(1212);
+
+            a.SortArray();
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
---
+++ CSharp/BubbleSort/SortBub.cs
@@ -0,0 +1,43 @@
+﻿namespace BubbleSort
+{
+    public class SortBub
+    {
+        private readonly int[] _arr;
+        private int _numItems;
+
+        public SortBub(int max)
+        {
+            _numItems = 0;
+            _arr = new int[max];
+        }
+
+        public void Insert(int value)
+        {
+            _arr[_numItems++] = value;
+        }
+
+        public void SortArray()
+        {
+            // A bubblesort will look backwards over the length of the array to bubble the tallest bugger.
+            // We decrement in order to ignore the last value which of course is the greatest.
+            for (int i = _numItems; i > 1; i--)
+            {
+                // Each iteration within an array will compare two items, 
+                // If the item on the left is greater than the item on the right then we swap them.
+                // This ensures after a full iteration of i the tallest item is bubbled to the right.
+                for (int j = 0; j < i - 1; j++)
+                {
+                    if (_arr[j] > _arr[j + 1])
+                        SwapItems(j);
+                }
+            }
+        }
+
+        private void SwapItems(int j)
+        {
+            var taller = _arr[j];
+            _arr[j] = _arr[j + 1];
+            _arr[j + 1] = taller;
+        }
+    }
+}
