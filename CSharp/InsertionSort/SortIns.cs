cc589cc809c81c52c0b315e37874b714e79d4ef4
Jason Linden <jason.linden@lexisnexis.co.za>
2020-03-02 15:31:06 +0200

    sort Ins ins ins

--- CSharp/InsertionSort/SortIns.cs
+++ CSharp/InsertionSort/SortIns.cs
@@ -13,23 +13,32 @@
 
         public void Insert(int value)
         {
+            // i++ means 'tell me the value of i, then increment'
+            // ++i means 'increment i, then tell me the value'
+
             _arr[_numItems++] = value;
         }
+
+        // The idea is to work through the array and partition the array into sorted on the left
+        // and unsorted on the right.
+
+        // We take out the left most unsorted item and compare it with the sorted partition (right of the item).
+        // This enables us to find where the item needs to go.
 
         public void SortArray()
         {
             for (int i = 1; i < _numItems; i++)
             {
                 var temp = _arr[i];
-                var counter = i;
+                var inner = i;
 
-                while (counter > 0 && _arr[counter - 1] >= temp)
+                while(inner > 0 && _arr[inner - 1] > temp)
                 {
-                    _arr[counter] = _arr[counter - 1];
-                    --counter;
-                };
+                    _arr[inner] = _arr[inner - 1];
+                    inner--;
+                }
 
-                _arr[counter] = temp;
+                _arr[inner] = temp;
             }
         }
     }
