b4639454eddc49559fa924a2d17ad5a836820f94
Jason Linden <jason.linden@lexisnexis.co.za>
2020-03-06 10:17:18 +0200

    stack ds and reverser algo

---
+++ CSharp/Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Please enter a string and press enter:");
+                var word = Console.ReadLine();
+
+                var reveser = new Reverser(word);
+                Console.Write(reveser.ReverseThis());
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharp/Stack/Reverser.cs
@@ -0,0 +1,37 @@
+﻿namespace Stack
+{
+    public class Reverser
+    {
+        private Stackx<string> _stacker;
+        private string _reverser;
+
+        public Reverser(string wordToReverseInto)
+        {
+            _reverser = wordToReverseInto;
+        }
+
+        public string ReverseThis()
+        {
+            InitiliazeStackx();
+
+            string result = string.Empty;
+
+            for (int i = 0; i < _reverser.Length; i++)
+            {
+                _stacker.Push(_reverser[i].ToString());
+            }
+
+            while (!_stacker.IsEmpty())
+            {
+                result += _stacker.Pop();
+            }
+
+            return result;
+        }
+
+        private void InitiliazeStackx()
+        {
+            _stacker = new Stackx<string>(_reverser.Length);
+        }
+    }
+}
