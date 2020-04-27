a6d4e44e6a34f91617a085535cc433375bc3a4c9
Jason Linden <jasonlinden01@gmail.com>
2020-04-27 20:53:18 +0200

    merge and hanoi

--- CSharp/TowersOfHanoi/Program.cs
+++ CSharp/TowersOfHanoi/Program.cs
@@ -10,7 +10,7 @@ namespace TowersOfHanoi
         }
 
         // We need to move n disks from A to C via B
-        // But we need to reduce the problem to 1 disk and move from there.
+        // But we need to reduce the problem to 2 disks and move from there.
         // As soon as the smallest disk is moved from A to B. we meed to move the second smallest to C and the smallest back on top (C).
         // That free's up the 3rd disk, which we move. Once we move n - 1 we need to rebuid the tree to allow us to move the final disk (n)
 
@@ -21,11 +21,12 @@ namespace TowersOfHanoi
                 Console.WriteLine($"Move disk 1 from {from} to {to}");
             else
             {
-                // Initial move
+                // Drill down to 1 disk to perform first move
                 Tower(disks - 1, from, to, via);
+                // Immediately move the second disk to intermediate column
                 Console.WriteLine($"Move disk {disks} from {from} to {to}");
 
-                // Rebuild tree
+                // Rebuild tree from disk 2 until n
                 Tower(disks - 1, via, from, to);
             }            
         }
---
+++ CSharp/mergesort/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace mergesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var unsortedArray = new int[] { 4, 65, 23, 55, 78, 3424, 56, 77, 2, 50, 89, 12 };
+
+            MergeSort(unsortedArray, 0, unsortedArray.Length - 1);
+
+            Console.ReadLine();
+        }
+
+        static void MergeSort(int[] array, int lowerBound, int upperBound)
+        {
+            // The base case occurs when the range contains only one element
+            if (lowerBound == upperBound)
+                return;
+            else
+            {
+                int mid = (lowerBound + upperBound) / 2;
+
+                // left hand side array
+                MergeSort(array, lowerBound, mid);
+
+                // right side of the array.
+                MergeSort(array, mid + 1, upperBound);
+
+                // Once we get here we have two sorted arrays ready to be merge.
+                Merge(array, lowerBound, mid, upperBound);
+            }
+        }
+
+        private static void Merge(int[] array, int lowerBound, int mid, int upperBound)
+        {
+            int leftTrackingIndex = 0;
+            int rightTrackingIndex = 0;
+            int lower = lowerBound;
+
+            // create two temp arrays from the ranges.
+            int left = mid - lowerBound + 1;
+            int[] leftArray = new int[left];
+
+            int right = upperBound - mid;
+            int[] rightArray = new int[right];
+
+            // Fill the temp arrays with data from original array
+            // loop of the range and insert from lower to mid range
+            for (int i = 0; i < left; i++)
+            {
+                leftArray[i] = array[lowerBound + i];
+            }
+
+            // loop of the range and insert from mid + 1 to upper range
+            for (int i = 0; i < right; i++)
+            {
+                rightArray[i] = array[mid + 1 + i];
+            }
+
+            // while loop to run from 0 until the size of the temp arrays
+            while (leftTrackingIndex < left && rightTrackingIndex < right)
+            {
+                // insert from the lowerbound and work up, sorting from left to right.
+                if (leftArray[leftTrackingIndex] < rightArray[rightTrackingIndex])
+                    array[lower++] = leftArray[leftTrackingIndex++];
+                else
+                    array[lower++] = rightArray[rightTrackingIndex++];
+            }
+
+            // if the left array is not empty
+            while (leftTrackingIndex < left)
+            {
+                array[lower++] = leftArray[leftTrackingIndex++];
+            }
+
+            while (rightTrackingIndex < right)
+            {
+                array[lower++] = rightArray[rightTrackingIndex++];
+            }
+        }
+
+        static void Merge(int[] final, int[] arrayB, int[] arrayC)
+        {
+            int sizeB = arrayB.Length;
+            int sizeC = arrayC.Length;
+
+            int aIndex = 0;
+            int bIndex = 0;
+            int cIndex = 0;
+
+            // while loop for both arrays.
+            // this loop will end if we reach the last index of one of the arrays
+            while (bIndex < sizeB && cIndex < sizeC)
+            {
+                if (arrayB[bIndex] < arrayC[cIndex])
+                    final[aIndex++] = arrayB[bIndex++];
+                else
+                    final[aIndex++] = arrayC[cIndex++];
+            }
+
+            // while loop if C is empty
+            while (bIndex < sizeB)
+            {
+                final[aIndex++] = arrayB[bIndex++];
+            }
+
+            // while loop if C is empty
+            while (cIndex < sizeC)
+            {
+                final[aIndex++] = arrayC[cIndex++];
+            }
+        }
+    }
+}
